e7c6e71d5c16c241b6fa4a3f2aa38f907603a7a2
xutianyi <xutianyi1999@live.com>
2024-03-11 18:04:52 +0800

    update

--- randrw-s3-client/examples/demo.rs
+++ randrw-s3-client/examples/demo.rs
@@ -1,5 +1,6 @@
 use std::io::Cursor;
 use std::str::FromStr;
+use rand::Rng;
 
 #[tokio::main]
 async fn main() {
@@ -34,20 +35,24 @@ async fn main() {
         }
         "get" => {
             let key = args.next().unwrap();
-            let offset = u64::from_str(&args.next().unwrap()).unwrap();
-            let size = u64::from_str(&args.next().unwrap()).unwrap();
+            let chunks = usize::from_str(&args.next().unwrap()).unwrap();
+            let mut rng = rand::thread_rng();
 
-            let offset2 = u64::from_str(&args.next().unwrap()).unwrap();
-            let size2 = u64::from_str(&args.next().unwrap()).unwrap();
+            let ranges = (0..32768).into_iter()
+                .map(|_| (rng.gen_range(0u64..1024 * 1024 * 1024), 32u64))
+                .collect::<Vec<_>>();
 
-            let parts = randrw_s3_client::get_object_with_ranges(
-                &key,
-                &[(offset, size), (offset2, size2)]
-            ).await.unwrap();
+            let mut futs = Vec::new();
 
-            for part in parts {
-                println!("offset: {}, data: {:?}", part.offset, part.data);
+            for chunk in ranges.chunks(chunks) {
+                let fut = async {
+                    randrw_s3_client::get_object_with_ranges(&key, chunk).await.unwrap()
+                };
+
+                futs.push(fut);
             }
+
+            futures_util::future::join_all(futs).await;
         }
         _ => unimplemented!()
     }
