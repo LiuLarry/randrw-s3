d56597a00d7c78215a23707b2c37fbfbc7a09e66
xutianyi <xutianyi1999@live.com>
2024-03-13 12:03:16 +0800

    update

--- randrw-s3-server/src/main.rs
+++ randrw-s3-server/src/main.rs
@@ -485,6 +485,7 @@ async fn get_object_with_ranges(
 
     let futs_res = futures_util::future::try_join_all(futs).await;
     let _ = notify.send(());
+    drop(notified);
     notify.closed().await;
 
     let parts;
